982dbec9ea8c93f62f879dea3a92677aca5267f1
CLIMAFORD <akelloharry1121@gmail.com>
2026-04-29 03:52:47 +0300

    Fix Firebase storage and Supabase init; prepare publish

--- js/firebaseConfig.js
+++ js/firebaseConfig.js
@@ -3,7 +3,7 @@ var firebaseConfig = {
     authDomain: "masenomap.firebaseapp.com",
     databaseURL: "https://masenomap-default-rtdb.firebaseio.com",
     projectId: "masenomap",
-    storageBucket: "masenomap.firebasestorage.app",
+    storageBucket: "masenomap.appspot.com",
     messagingSenderId: "261705653399",
     appId: "1:261705653399:web:7482be963b286d645c9dd2",
     measurementId: "G-5NH6G4108J"
